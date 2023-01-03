962d38eab56323f3414c11fbb6810f066ccdb8b8
Sonu <sonualam1599@gmail.com>
2023-01-03 23:51:38 +0530

    Level 2 - Database Encryption

--- Secrets/app.js
+++ Secrets/app.js
@@ -3,8 +3,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const mogoose = require("mongoose");
-const { default: mongoose } = require("mongoose");
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 
@@ -19,10 +19,13 @@ mongoose.set('strictQuery', true);
 mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
 
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
     email: String,
     password: String
-};
+});
+
+const secret = "Thisisourlittlesecret.";
+userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
